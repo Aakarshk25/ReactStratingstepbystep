deefbf2e3cfabdf3b66b2cf026fba8965d1123b4
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2024-04-20 22:58:17 +0530

    Update index.js

--- 3rdDay/src/index.js
+++ 3rdDay/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 const customStyle = {color :"red",
 }
 
-
+//hiii
 ReactDOM.render(
   <h1 style={customStyle}>Hello World!</h1>,
   document.getElementById("root")
