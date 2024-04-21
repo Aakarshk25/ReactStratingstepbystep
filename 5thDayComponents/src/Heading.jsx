fa4b70fcddfd473386ad69ed5fffc63a580991de
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2024-04-22 00:04:20 +0530

    Add files via upload

---
+++ 5thDayComponents/src/Heading.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PropTypes from "prop-types"; // i don't know the use till yet
+
+function Heading(props) {
+  return (
+    <div>
+      <h1>My Favourite Foods</h1>
+      <p>for showing how to export more things in single heading</p>
+    </div> //div banake tu 2 se 3 chhiz return krr skete ho
+  );
+}
+
+export default Heading;
