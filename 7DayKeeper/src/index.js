3bbae5c6bc0de94fff175c37bdafdbf61039fa39
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2024-04-24 14:50:03 +0530

    Update index.js

--- 7DayKeeper/src/index.js
+++ 7DayKeeper/src/index.js
@@ -7,7 +7,7 @@
 //5. Create a Note.jsx component to show a <div> element with a
 //<h1> for a title and a <p> for the content.
 //6. Make sure that the final website is styled like the example shown here:
-//https://l1pp6.csb.app/
+//https://l1pp6.csb.app/ h
 
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from "react";
