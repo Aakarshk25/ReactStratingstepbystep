391f597dc0790016ef06dbea7ccb5751cbe13d1c
Aakarsh Srivastava <132938129+Aakarshk25@users.noreply.github.com>
2024-07-16 03:40:03 +0530

    Update Login.jsx

--- 13challege302sosimple/src/components/Login.jsx
+++ 13challege302sosimple/src/components/Login.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Input from "./Input";
 
 function Login() {
   return (
     <form className="form">
-      <button type="Login">Login</button>
+      <Input type="text" placeholder="Username" />
+      <Input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
     </form>
   );
 }
